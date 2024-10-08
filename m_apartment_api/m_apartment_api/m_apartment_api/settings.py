e5774d0ef027fbea5d71287d04a4886f643fba7e
minhliem03 <93208245+tnminhliem@users.noreply.github.com>
2024-10-08 19:41:05 +0700

    deploy

--- m_apartment_api/m_apartment_api/m_apartment_api/settings.py
+++ m_apartment_api/m_apartment_api/m_apartment_api/settings.py
@@ -65,6 +65,7 @@ AUTH_USER_MODEL = 'apartment.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -159,12 +160,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
